b14d59a4cd7138e368ab43439c06a1ad134745c6
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-11-25 01:41:08 -0800

    video bug

--- app/assets/javascripts/components/Photos/photo_index.js.jsx
+++ app/assets/javascripts/components/Photos/photo_index.js.jsx
@@ -17,21 +17,27 @@ var PhotoIndex = React.createClass({
 
   render: function() {
     var idx = 0;
-
+    var video;
     return (
       <div className="container photo-index">
         {
           this.state.photos.map(function(photo) {
             idx++;
+            video = (photo.type === "video");
+            if (!video) {
+              photo.videos = { standard_resolution: null };
+            }
             return (
               <Photo caption={photo.caption.text}
-                     comments={photo.comments.data}
-                     tags={photo.tags}
-                     url={photo.images.standard_resolution.url}
-                     likes={photo.likes.count}
-                     user={photo.user.username}
-                     idx={idx}
-                     key={idx} />
+                       comments={photo.comments.data}
+                       tags={photo.tags}
+                       url={photo.images.standard_resolution.url}
+                       likes={photo.likes.count}
+                       user={photo.user.username}
+                       idx={idx}
+                       vid_url={photo.videos.standard_resolution}
+                       video={video}
+                       key={idx} />
             );
           })
         }
--- app/assets/javascripts/components/Photos/photo_item.js.jsx
+++ app/assets/javascripts/components/Photos/photo_item.js.jsx
@@ -10,17 +10,24 @@ var Photo = React.createClass({
     var comments = this.props.comments;
     var tags = this.props.tags;
     var url = this.props.url;
+    var vid_url = this.props.vid_url;
     var likes = this.props.likes;
     var user = this.props.user;
     var account = "https://www.instagram.com/" + user;
     var idx = this.props.idx;
+    var media;
+
+    if (this.props.video) {
+      Media = React.createElement('video', { src: vid_url, className: media });
+    } else {
+      Media = React.createElement('img', { src: vid_url, className: media });
+    }
 
     return (
       <div className="photo-item">
-        <img src={url} className="image"/>
+        <Media/>
         <br/>
         <a onClick={this._profileHandler} href={account}> {user} </a>
-        <br/>
         <br/>
         <div> { caption } </div>
         <br/>
