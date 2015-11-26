59176bc9de146e571f64720095f9abf246819c8a
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-11-25 17:54:32 -0800

    create image model method

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -16,12 +16,37 @@ class Api::PhotosController < ApplicationController
       response = JSON.parse(HTTParty.get(url).body)
     end
 
+    search_session = SecureRandom.urlsafe_base64
+
+    response['data'].each do |image|
+      create_image(image, search_session)
+    end
+
     render json: response
   end
 
   private
+
   def timestampify(date)
     date_arr = date.split("-").map(&:to_i)
     Date.new(date_arr[0], date_arr[1], date_arr[2]).to_time.to_i
   end
+
+  def create_image(image, search_session_token)
+    object = Image.new( search_session: search_session_token,
+                        tags: image['tags'],
+                        username: image['user']['username'],
+                        image_url: image['images']['standard_resolution']['url'],
+                        timestamp: image['created_time'],
+                        caption: image['caption']['text'],
+                        likes: image['likes']['count'],
+                        comment_count: image['comments']['count'],
+                        comments: image['comments']['data']
+                        )
+
+    object.video_url = image['videos']['standard_resolution']['url'] if image['videos']
+
+    object.save!
+  end
+
 end
