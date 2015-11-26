982f47174bbc23aa81aded0e9e220d50fd49df89
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-11-25 19:12:31 -0800

    paginate button css

--- app/assets/javascripts/components/Photos/photo_index.js.jsx
+++ app/assets/javascripts/components/Photos/photo_index.js.jsx
@@ -1,6 +1,6 @@
 var PhotoIndex = React.createClass({
   getInitialState: function() {
-    return { photos: PhotoStore.all() };
+    return { photos: PhotoStore.all(), page: 1 };
   },
 
   componentDidMount: function() {
@@ -15,32 +15,52 @@ var PhotoIndex = React.createClass({
     this.setState( { photos: PhotoStore.all() });
   },
 
+  disableBack: function() {
+    return (this.state.page === 1);
+  },
+
+  disableForward: function() {
+    return (this.state.photos.length === 0);
+  },
+
   render: function() {
     var idx = 0;
     var video;
     return (
-      <div className="container photo-index">
-        {
-          this.state.photos.map(function(photo) {
-            idx++;
-            video = (photo.type === "video");
-            if (!video) {
-              photo.videos = { standard_resolution: null };
-            }
-            return (
-              <Photo caption={photo.caption.text}
-                       comments={photo.comments.data}
-                       tags={photo.tags}
-                       url={photo.images.standard_resolution.url}
-                       likes={photo.likes.count}
-                       user={photo.user.username}
-                       idx={idx}
-                       vid_url={photo.videos.standard_resolution}
-                       video={video}
-                       key={idx} />
-            );
-          })
-        }
+      <div>
+        <div className="container page-button">
+          <div className="col-md-6 text-center">
+            <button className='btn btn-default'
+                    disabled={this.disableBack()}> Back </button>
+          </div>
+          <div className="col-md-6 text-center">
+            <button className='btn btn-default'
+                    disabled={this.disableForward()}> Forward </button>
+          </div>
+        </div>
+        <div className="photo-index container">
+          {
+            this.state.photos.map(function(photo) {
+              idx++;
+              video = (photo.type === "video");
+              if (!video) {
+                photo.videos = { standard_resolution: null };
+              }
+              return (
+                <Photo caption={photo.caption.text}
+                         comments={photo.comments.data}
+                         tags={photo.tags}
+                         url={photo.images.standard_resolution.url}
+                         likes={photo.likes.count}
+                         user={photo.user.username}
+                         idx={idx}
+                         vid_url={photo.videos.standard_resolution}
+                         video={video}
+                         key={idx} />
+              );
+            })
+          }
+        </div>
       </div>
     );
   },
