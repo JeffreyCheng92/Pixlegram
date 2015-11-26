d7d818d3eec20a76a49c9f38613bbb2e470dd179
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-11-25 17:18:24 -0800

    migration for images

---
+++ db/migrate/20151126005230_create_images.rb
@@ -0,0 +1,20 @@
+class CreateImages < ActiveRecord::Migration
+  def change
+    create_table :images do |t|
+      t.string :username, null: false
+      t.string :tags, array: true, default: []
+      t.string :image_url, null: false
+      t.string :video_url
+      t.string :timestamp, null: false
+      t.string :caption
+      t.integer :likes
+      t.integer :comment_count
+      t.string :comments, array: true, default: []
+      t.string :search_session, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :images :search_session
+  end
+end
